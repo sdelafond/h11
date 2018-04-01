6d8d1e50343d31e9a191779ddc8d32cf323faefc
Sébastien Delafond <sdelafond@gmail.com>
2018-04-01 11:22:08 +0200

    New upstream version 0.8.0

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+# defines __version__
+exec(open("h11/_version.py").read())
+
+setup(
+    name="h11",
+    version=__version__,
+    description=
+        "A pure-Python, bring-your-own-I/O implementation of HTTP/1.1",
+    long_description=open("README.rst").read(),
+    author="Nathaniel J. Smith",
+    author_email="njs@pobox.com",
+    license="MIT",
+    packages=find_packages(),
+    url="https://github.com/njsmith/h11",
+    # This means, just install *everything* you see under h11/, even if it
+    # doesn't look like a source file, so long as it appears in MANIFEST.in:
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: System :: Networking",
+    ],
+)
